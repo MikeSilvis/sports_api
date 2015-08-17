c1219494135f041d7c363502870a44aeae8c5946
Mike Silvis <mikesilvis@gmail.com>
2015-08-17 09:14:48 -0700

    adding mlb

--- lib/sports_api/fetchers/boxscores/leagues/mlb.rb
+++ lib/sports_api/fetchers/boxscores/leagues/mlb.rb
@@ -4,6 +4,34 @@ class SportsApi::Fetcher::Boxscore::MLB < SportsApi::Fetcher::Boxscore
   end
 
   def generate_score_detail
+    Array(SportsApi::Model::ScoreDetail.new.tap do |score_detail|
+      score_detail.contents = generate_score_details
+    end)
+  end
+
+  def generate_score_details
+    table = markup.css('.mod-container.mod-open.mod-open-gamepack')[4]
+    table.css('tbody tr.even, tr.odd').map do |row|
+      SportsApi::Model::ScoreDetailContent.new.tap do |detail_content|
+        detail_content.time = row.css('td')[1]
+        detail_content.detail = row.css('td')[2].content
+
+        css_klass = row.css('td')[0].at_css('div').attributes['class'].value.match(/mlb-small-\d*/).to_s
+        detail_content.competitor = css_klass == home_klass ? score.competitors.first : score.competitors.last
+      end
+    end
+  end
+
+  def home_klass
+    get_klass(2)
+  end
+
+  def away_klass
+    get_klass(0)
+  end
+
+  def get_klass(position)
+    markup.css('.team-color-strip')[position].at_css('th div').attributes['class'].value.match(/mlb-small-\d*/).to_s
   end
 
   def markup
--- spec/fetchers/boxscores/leagues/mlb_spec.rb
+++ spec/fetchers/boxscores/leagues/mlb_spec.rb
@@ -4,12 +4,18 @@ describe SportsApi::Fetcher::Boxscore::MLB do
   describe '#find' do
     let(:gameid) { 350816114 }
     let(:json_stub) { StubbedJson.get('postgame.json') }
-    let(:html_stub) { StubbedHtml.get('400791773.html') }
+    let(:html_stub) { StubbedHtml.get('350816114.html') }
     let(:find) { SportsApi::Fetcher::Boxscore::MLB.find(gameid) }
     before { expect_any_instance_of(SportsApi::Fetcher::Score::MLB).to receive(:get).with('baseball', 'mlb', dates: 20150816).and_return(json_stub) }
+    before { expect_any_instance_of(SportsApi::Fetcher::Boxscore::MLB).to receive(:get).with('mlb/boxscore', gameId: gameid).and_return(html_stub) }
 
     context 'event info' do
+      let(:score_detail) { find.score_details.first }
       it { expect(find.score.date).to eq(Date.new(2015, 8, 16)) }
+      it { expect(find.score_details.count).to eq(1) }
+      it { expect(score_detail.headline).to eq(nil) }
+      it { expect(score_detail.contents.first.detail).to eq('Donaldson singled to right, Tulowitzki scored.') }
+      it { expect(score_detail.contents.first.competitor.abbreviation).to eq('TOR') }
     end
   end
 end
