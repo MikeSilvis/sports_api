2d1637268b3ca6a107f0ae7a1c1cf230ee0c846f
Mike Silvis <mikesilvis@gmail.com>
2015-09-07 09:24:00 -0700

    adding calendar shortcut

--- lib/sports_api/fetchers/calendar/calendar.rb
+++ lib/sports_api/fetchers/calendar/calendar.rb
@@ -1,2 +1,14 @@
 class SportsApi::Fetcher::Calendar
+  def self.find(league, gameid)
+    case league
+    when SportsApi::NCF
+      SportsApi::Fetcher::Calendar::NFL.find(gameid)
+    when SportsApi::NCF
+      SportsApi::Fetcher::Calendar::NCF.find(gameid)
+    when SportsApi::NBA
+      SportsApi::Fetcher::Calendar::NBA.find(gameid)
+    when SportsApi::MLB
+      SportsApi::Fetcher::Calendar::MLB.find(gameid)
+    end
+  end
 end
