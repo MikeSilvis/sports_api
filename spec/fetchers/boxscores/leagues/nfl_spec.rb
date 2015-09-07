1ca8ef7a63d0edf1fef659e282abf4ebe5e6bf7f
Mike Silvis <mikesilvis@gmail.com>
2015-09-07 08:59:31 -0700

    adding more tests for nfl boxscore

--- spec/fetchers/boxscores/leagues/nfl_spec.rb
+++ spec/fetchers/boxscores/leagues/nfl_spec.rb
@@ -8,8 +8,8 @@ describe SportsApi::Fetcher::Boxscore::NFL do
     let(:json_stub_week_2) { StubbedJson.get('postgame-week-2.json') }
     let(:html_stub) { StubbedHtml.get('400791773.html') }
     let(:expect_instance) { allow_any_instance_of(SportsApi::Fetcher::Score::NFL) }
-    before { expect_instance.to receive(:get).with('football', 'nfl', week: 1, seasontype: 1).and_return(json_stub_week_2) }
-    before { expect_instance.to receive(:get).with('football', 'nfl', week: 5, seasontype: 1).and_return(json_stub_week_2) }
+    before { expect_instance.to receive(:get).with('football', 'nfl', week: 1, seasontype: 1).and_return(json_stub_week_1) }
+    before { expect_instance.to receive(:get).with('football', 'nfl', week: 2, seasontype: 1).and_return(json_stub_week_2) }
     before { expect_any_instance_of(SportsApi::Fetcher::Boxscore::NFL).to receive(:get).with('nfl/game', gameId: gameid).and_return(html_stub) }
 
     context 'event info' do
@@ -17,6 +17,8 @@ describe SportsApi::Fetcher::Boxscore::NFL do
       it { expect(find.score.date).to eq(Date.new(2015, 8, 16)) }
       it { expect(find.score_details.count).to eq(4) }
       it { expect(score_detail.headline).to eq('First Quarter') }
+      it { expect(score_detail.contents.first.detail).to eq('Adam Vinatieri 32 Yd Field Goal') }
+      it { expect(score_detail.contents.first.competitor.abbreviation).to eq('IND') }
     end
   end
 end
